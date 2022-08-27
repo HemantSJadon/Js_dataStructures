ecfe724cf5511197c46212af63eafecc6286da5c
jadonhs185 <h.s.jadon185@gmail.com>
2022-08-27 14:13:05 +0530

    add binary tree algo section

---
+++ ds/binaryTree/bt_dfs.js
@@ -0,0 +1,89 @@
+//Problem: https://structy.net/problems/depth-first-values
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const test1 = () => {
+    const a = new Node('a');
+    const b = new Node('b');
+    const c = new Node('c');
+    const d = new Node('d');
+    const e = new Node('e');
+    const f = new Node('f');
+
+    a.left = b;
+    a.right = c;
+    b.left = d;
+    b.right = e;
+    c.right = f;
+
+    //     a
+    //    /  \
+    //    b   c
+    //   / \   \
+    //  d  e    f
+
+    console.log(depthFirstValues_iterative(a));
+}
+
+const test2 = () => {
+    console.log(depthFirstValues_iterative(null));
+}
+
+const test3 = () => {
+    const a = new Node('a');
+    const b = new Node('b');
+    const c = new Node('c');
+    const d = new Node('d');
+    const e = new Node('e');
+
+    a.right = b;
+    b.left = c;
+    c.right = d;
+    d.right = e;
+            // a
+            //  \
+            //   b
+            //  /
+            // c
+            //  \
+            //   d
+            //    \
+            //     e
+    console.log(depthFirstValue_recursive(a));
+}
+
+
+//Time and space complexity: O(n) linear since each node is visited once and removed from stack only once n = number of nodes in the tree
+const depthFirstValues_iterative = function (root) {
+    const traversed = [];
+    if (root !== null) {
+        const stack = [root];
+        while (stack.length > 0) {
+            const current = stack.pop();
+            const { val, left, right } = current;
+            traversed.push(val);
+            if (right !== null) stack.push(right);
+            if (left !== null) stack.push(left);
+        }
+    }
+    return traversed;
+};
+
+const depthFirstValue_recursive = function(node, traversed = []){
+    if(node === null) return;
+    const {val, left, right} = node;
+    traversed.push(val);
+    depthFirstValue_recursive(left, traversed);
+    depthFirstValue_recursive(right, traversed);
+    return traversed;
+}
+
+test1();
+test2();
+test3();
